1a1763f5cf0cad0f175f366116b7fe2c06232a57
ordelore <ordelore@gmail.com>
2014-04-18 16:32:50 -0500

    Full source and binaries
    Bugged source. if you run this on a  3.6 OS Nspire, then it crashes your
    calc. don't know about 3.1, though.

--- CLI.c
+++ CLI.c
@@ -5,16 +5,51 @@ int main(void) {
     nio_console csl;
     lcd_ingray();
     clrscr();
+	
     // 53 columns, 29 rows. 0px offset for x/y.
     // Background color 0 (black), foreground color 15 (white)
-    nio_InitConsole(&csl, 53, 29, 0, 0, 0, 15);
+    nio_InitConsole(&csl, 53, 29, 0, 0, 0, 12);
     nio_DrawConsole(&csl);
-    nio_printf(&csl, "nCLI");
-	nio_printf(&csl, "ordelore8x");
-	nio_printf(&csl, "This software is distibuted with NO WARRANTY");
-	nio_printf(&csl, "Use at your own risk!");
-	nio_printf(&csl, "moo");
-    wait_key_pressed();
-    nio_CleanUp(&csl);
-    return 0;
+    nio_printf(&csl, "nCLI\n");
+	nio_printf(&csl, "ordelore8x\n");
+	nio_printf(&csl, "This software is distibuted with NO WARRANTY\n");
+	nio_printf(&csl, "Use at your own risk!\n");
+	nio_printf(&csl, "moo\n");
+    nio_printf(&csl, "type 'help' for help\n");
+	nio_printf(&csl, "This is in no way coded similar to Linux\n");
+	nio_printf(&csl, "\n");
+	
+	while(1)
+	{
+		char text[100];
+		nio_printf(&csl, ">");
+		
+		if(!nio_gets(text)) {
+		continue;
+		}
+	if (!strcmp(text, "help")) {
+		nio_printf(&csl, "'exit': quit\n");
+		nio_printf(&csl, "Find the other commands!\n");
+	}
+	if (!strcmp(text, "exit")) {
+		nio_CleanUp(&csl);
+		return 0;
+    }
+	if (!strcmp(text, "make me a sandwich")) {
+		nio_printf(&csl, "NO! Make one yourself\n");
+	;}
+	if (!strcmp(text, "sudo make me a sandwich")) {
+		nio_printf(&csl, "ok\n");
+	}
+	if (!strcmp(text, "cemetech")) {
+		nio_printf(&csl, "Leading the Way to the Future\n");
+	}
+	if (!strcmp(text, "omnimaga")) {
+		nio_printf(&csl, "Lobsters\n");
+	}
+	if (!strcmp(text, "cake")) {
+		nio_printf(&csl, "Lies, all Lies\n");
+	}
+	return 0;
+}
 }
